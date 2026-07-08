4710323a93cb6e6c5bbe8528b239bed35f5471ef
Luis Cutz <luis_cm05@hotmail.com>
2026-07-08 18:11:33 -0500

    add(api): Add rooms and organizations to api router

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1.routes import documents
-from app.api.v1.routes import auth
+from app.api.v1.routes import auth, documents, rooms, organizations
 
 api_router = APIRouter()
 
 api_router.include_router(documents.router, prefix="/documents", tags=["Documents"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(rooms.router, prefix="/rooms", tags=["Rooms"])
+api_router.include_router(organizations.router, prefix="/organizations", tags=["Organizations"])
