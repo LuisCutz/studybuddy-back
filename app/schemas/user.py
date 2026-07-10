47a89c9ff230d0dcf0eefec380fc8fb67560600e
Luis Cutz <luis_cm05@hotmail.com>
2026-07-10 10:05:32 -0500

    add(auth): Reset password endpoint

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -40,3 +40,8 @@ class ForgotPasswordRequest(BaseModel):
 class ResetPasswordRequest(BaseModel):
     token: str
     new_password: str
+
+
+class ChangePasswordRequest(BaseModel):
+    current_password: str
+    new_password: str
